0e3fe1859cfe4bb39d74715e7ea1ecd96efc1f10
Nicky <nicky@aforapple.com>
2024-08-27 14:17:12 +0530

    Person model created

--- home/views.py
+++ home/views.py
@@ -2,11 +2,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(['Get']) #Pass the menthod that this function accepts
+@api_view(['Get', 'POST']) #Pass the menthod that this function accepts
 def index(request):
     courses = {
         'course_name' : 'Python',
         'Learn' : ['Flask', 'Django', 'FastAPI'],
         'Duration' : '20 Hours'
     }
-    return Response(courses)
+
+    if request.method == 'GET':
+        print('You requested a GET method')
+        return Response(courses)
+    elif request.method == 'POST':
+        print('You requested a POST method')
+        return Response(courses)
